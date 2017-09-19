1594444711a1b7de24986d8cde1021edbda27f18
DrnSmltr <30936251+DrnSmltr@users.noreply.github.com>
2017-09-20 00:03:35 +0200

    Initial commit

---
+++ app/src/main/java/com/drnsmltr/opendronesimulatorapi/model/LEDStatus.java
@@ -0,0 +1,11 @@
+package com.drnsmltr.opendronesimulatorapi.model;
+
+/**
+ * Represents the LED Status of the simulated drone
+ *
+ */
+
+public enum LEDStatus
+{
+    ACTIVATED, DEACTIVATED, UNKNOWN;
+}
